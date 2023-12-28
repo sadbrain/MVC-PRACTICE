633281ce9110be0bfa9f3ef59bf42d7a7c8ddb22
sadbrain <riotgheptrannhulol@gmail.com>
2023-12-27 21:31:47 -0800

    first commit

---
+++ controller/home/index.controller.php
@@ -0,0 +1,2 @@
+<?php
+require dirname(dirname(__DIR__)) . "/views/home/index.view.php";
---
+++ views/home/index.view.php
@@ -0,0 +1,12 @@
+<?php require   dirname(__DIR__) . "/partials/head.php" ?>
+<?php require dirname(__DIR__) . "/partials/nav.php" ?>
+
+<main>
+    <div class="p-3">
+        <!-- Replace with your content -->
+        <p>Hello Home</p>
+        <!-- /End replace -->
+    </div>
+</main>
+
+<?php require dirname(__DIR__) . "/partials/footer.php" ?>
